32bd2dae30abe7e2ca94ffae2dc3d9d60ae01c60
Luis <luisrc91@live.com>
2023-03-18 11:32:30 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import data
-
+import Gallery from './components/Gallery'
 // Import components
 import './App.css';
 
@@ -8,10 +8,17 @@ function App(){
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+      <Gallery />      
       </div>
     </div>
   );
 }
 
+
+
+// function App(){
+//   return (
+//     <Gallery />
+//   )
+// }
 export default App;
